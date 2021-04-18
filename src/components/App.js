bb8316c4a1f8faccb868ca8a55a2dcc34373fbdf
Wrokbrook repo <chukschiboy@gmail.com>
2021-04-18 05:17:26 -0700

    creating the front-end to ChyFarm token

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0fff'
     }
   }
 
@@ -20,7 +20,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="#"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
