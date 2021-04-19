e93eb7239061ffba5f37e5b7ca890daa6badaf58
Wrokbrook repo <chukschiboy@gmail.com>
2021-04-19 12:00:14 -0700

    creating main.js file to interact with the entry file of app.js

---
+++ src/components/Main.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import ChyToken from '../abis/ChyToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
+import Navbar from './Navbar'
+import './App.css'
+
+class Main extends Component {
+
+  // Calling the function of web3
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  // Defining load block chain data
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+
+    // Load DaiToken
+    const daiTokenData = DaiToken.networks[networkId]
+    if(daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+      this.setState({ daiToken })
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString() })
+    } else {
+      window.alert('DaiToken contract not deployed to detected network.')
+    }
+
+    // Load ChyToken
+    const chyTokenData = ChyToken.networks[networkId]
+    if(chyTokenData) {
+      const chyToken = new web3.eth.Contract(ChyToken.abi, chyTokenData.address)
+      this.setState({ chyToken })
+      let chyTokenBalance = await chyToken.methods.balanceOf(this.state.account).call()
+      this.setState({ chyTokenBalance: chyTokenBalance.toString() })
+    } else {
+      window.alert('ChyToken contract not deployed to detected network')
+    }
+
+    // Load TokenFarm
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if(tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+      this.setState({ tokenFarm })
+      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+    } else {
+      window.alert('TokenFarm contract not deployed to detected network.')
+    }
+
+    this.setState({ loading: false })
+
+  }
+
+  
+  // Using this async function to connect the app to the block chain
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0ff',
+      daiToken: {},
+      chyToken: {},
+      tokenFarm: {},
+      daiTokenBalance: '0',
+      chyTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                </a>
+
+                <h1>Hello, World!</h1>
+
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
